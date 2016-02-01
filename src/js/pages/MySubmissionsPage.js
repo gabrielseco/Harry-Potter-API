92491301da0becb737ec058b4ba571fa260c72fe
Missy Bur <missybur@gmail.com>
2016-02-01 04:07:40 -0800

    Make changes to styling

--- src/js/pages/MySubmissionsPage.js
+++ src/js/pages/MySubmissionsPage.js
@@ -8,62 +8,64 @@ export default class MySubmissionsPage extends UserComponent {
       <DocumentTitle title={`MySubmissionsPage`}>
         <div className="container submissions">
                    <div className="row">
+                   <h3>My Submissions</h3>
+              <hr />
             <ul className="list-group">
               <li className="list-group-item">
                 <strong>First Name</strong>
-                <span className="pull-right">{ this.state.user.givenName }</span>
+                <span className="pull-right">Harry</span>
               </li>
               <li className="list-group-item">
                 <strong>Last Name</strong>
-                <span className="pull-right">{ this.state.user.surname }</span>
+                <span className="pull-right">Potter</span>
               </li>
               <li className="list-group-item">
                 <strong>Middle Name</strong>
-                <span className="pull-right">{ this.state.user.middleName }</span>
+                <span className="pull-right">James</span>
               </li>
               <li className="list-group-item">
                 <strong>DOB</strong>
-                <span className="pull-right">{ this.state.user.dob }</span>
+                <span className="pull-right">MAy 1st 1999</span>
               </li>
               <li className="list-group-item">
                 <strong>DOD</strong>
-                <span className="pull-right">{ this.state.user.dod }</span>
+                <span className="pull-right">Harry never dies!</span>
               </li>
               <li className="list-group-item">
                 <strong>Gender</strong>
-                <span className="pull-right">{ this.state.user.gender }</span>
+                <span className="pull-right">Male</span>
               </li>
               <li className="list-group-item">
                 <strong>House</strong>
-                <span className="pull-right">{ this.state.user.house }</span>
+                <span className="pull-right">Gryffindor</span>
               </li>
               <li className="list-group-item">
                 <strong>Magical</strong>
-                <span className="pull-right">{ this.state.user.magical }</span>
+                <span className="pull-right">True</span>
               </li>
               <li className="list-group-item">
                 <strong>Muggle Born</strong>
-                <span className="pull-right">{ this.state.user.muggleBorn }</span>
+                <span className="pull-right">False</span>
               </li>
               <li className="list-group-item">
                 <strong>Death Eater</strong>
-                <span className="pull-right">{ this.state.user.DA }</span>
+                <span className="pull-right">False</span>
               </li>
               <li className="list-group-item">
                 <strong>Special Skills</strong>
-                <span className="pull-right">{ this.state.user.specialSkills }</span>
+                <span className="pull-right">Defence Against the Dark Arts; Flying; Surviving Avada Kedavras</span>
               </li>
               <li className="list-group-item">
                 <strong>Occupation</strong>
-                <span className="pull-right">{ this.state.user.ocupation }</span>
+                <span className="pull-right">auror</span>
               </li>
               <li className="list-group-item">
                 <strong>Wand</strong>
-                <span className="pull-right">{ this.state.user.wand }</span>
+                <span className="pull-right">11 inch Holly, phoenix feather core</span>
               </li>
               <li className="list-group-item">
                 <strong>Patronus</strong>
-                <span className="pull-right">{ this.state.user.patronous }</span>
+                <span className="pull-right">doe</span>
               </li>
             </ul>
           </div>
