0101390acc5bd741b50ffe84d4e3f454430a59eb
Missy Bur <missybur@gmail.com>
2016-01-31 22:10:07 -0800

    Make changes to routes

--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -3,17 +3,17 @@ import React, { PropTypes } from 'react';
 import DocumentTitle from 'react-document-title';
 import { LoginLink } from 'react-stormpath';
 
-import Header from './components/Header';
+// import Header from './components/Header';
+// import GraphQL from './components/GraphqlIde'
+// import infoBlurb from './components/infoBlurb'
 
 
-export default class MasterPage extends React.Component {
+export default class HomePage extends React.Component {
   render() {
     console.log(this.props.children)
     return (
-      <DocumentTitle title='Stormpath Express + React Example'>
-        <div className='MasterPage'>
-          <Header />
-          { this.props.children }
+      <DocumentTitle title='HomePage'>
+        <div className='HomePage'>
         </div>
       </DocumentTitle>
     );
--- src/js/pages/IndexPage.js
+++ src/js/pages/IndexPage.js
@@ -16,12 +16,49 @@ export default class IndexPage extends React.Component {
           <ol className="lead">
             <li><Link to="/register">Registration</Link></li>
             <li><LoginLink /></li>
-            <li><Link to="/forgot">Forgot Password</Link></li>
             <li><Link to="/reviewSubmissionsPage">Submissions</Link></li>
             <li><Link to="/mySubmissionsPage">My Submissions</Link></li>
           </ol>
         </div>
-      </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <h3>GraphQL</h3>
+              <hr />
+            </div>
+          </div>
+            <iframe className="col-md-8 api" id="graphQL" src="http://localhost:3000/graphql" width="100%" height="500" />
+          </div>
+
+          <div className="container bottomInfo">
+            <div className="row pad_bot">
+              <div className="col-sm-1 col-lg-1 col-md-1"></div>
+                <div className="col-sm-3 col-lg-3 col-md-3">
+                  <h3 className="center">What Is This?</h3>
+                    <p>
+                      The Harry Potter API, or "HP-API" is the first data source that allows fans to add information into the database.  The information that you sumbit will be verified by Harry Potter fans who use this site and permantly remain on the database.
+                    </p>
+                </div>
+
+                <div className="col-sm-4 col-lg-4 col-md-4">
+                  <h3 className="center">How Can I Use This?</h3>
+                    <p>
+                    The HP-API database is generated soley by our users of the site who have registered.  If you would like to just view the data, feel free to check out our Submissions.
+                    </p>
+
+                    <p>
+                    If you would like to start entering submissions into the data base, follow the steps listed in our directions up at the top of the page.  Once you have registered an account, you are able to submit data.  After submitting data that is approved by other users on the database, you will be given permission to also authorize other users submissions.
+                    </p>
+
+                    <p>
+                    Thank you for visiting our site, we hope you enjoy!
+                    </p>
+                </div>
+
+            </div>
+          </div>
+        </div>
     );
   }
 }
